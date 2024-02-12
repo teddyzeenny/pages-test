2c947dc9fff1b8d3b936078cdc3abcb141ed9fe4
Teddy Zeenny <teddyzeenny@gmail.com>
2024-02-12 21:57:24 +0200

    Testing ejs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: "/pages-test/",
+  plugins: [
+    react(),
+    ViteEjsPlugin({
+      configValue: "Config Value",
+    }),
+  ],
 });
